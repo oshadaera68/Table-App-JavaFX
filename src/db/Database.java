2aa0f11099ca6a7f5cf0ad89bc1c526e028ffca1
oshadaera68 <mindartlk010@gmail.com>
2022-01-13 15:11:23 +0530

    added model class and db class

---
+++ src/db/Database.java
@@ -0,0 +1,13 @@
+/**
+ * Powered By : MINDARTLK.
+ * version:0.1.0
+ */
+package db;
+
+import model.Customer;
+
+import java.util.ArrayList;
+
+public class Database {
+    public static ArrayList<Customer> customerTable  = new ArrayList<Customer>();
+}
